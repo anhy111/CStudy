d808ceed1aef15c0d6deca8bef42ca13478f9ae3
anhy111 <gkdyd123@gmail.com>
2023-03-04 00:06:18 +0900

    3/03

---
+++ src/struct/rectangle/shape.c
@@ -0,0 +1,15 @@
+#include "shape.h"
+
+rectangle_t build_rectangle(point_t p0, point_t p1)
+{
+	
+}
+
+int get_line_length_sq(line_t line)
+{
+	
+}
+int get_rectangle_area(rectangle_t rect)
+{
+	
+}
